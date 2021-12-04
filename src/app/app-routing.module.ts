39e3a0ad4c7b0d5033b419d2cb163182c1790dcf
Nawod <madhuvanthanawo1@gmail.com>
2021-12-04 16:45:55 +0530

    Initial commite

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { CartComponent } from './component/cart/cart.component';
+import { ProductComponent } from './component/product/product.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'product',pathMatch:'full'},
+  {path:'product', component: ProductComponent},
+  {path:'cart', component: CartComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/cart/cart.component.ts
@@ -0,0 +1,30 @@
+import { CartService } from './../../service/cart.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent implements OnInit {
+
+  public product : any = [];
+  public grandTotal !: number;
+
+  constructor(private cartService : CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.getProduct()
+    .subscribe(res=>{
+      this.product = res;
+      this.grandTotal = this.cartService.getTotalPrice();
+    })
+  }
+  removeItem(item: any){
+    this.cartService.removeCartItem(item);
+  }
+  emptyCart(){
+    this.cartService.removeAllCart();
+  }
+
+}
